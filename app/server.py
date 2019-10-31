2b38959ebfe8c015bcb68b7d572d04305123aaf6
mundmc <drhappypills@hotmail.com>
2019-10-31 14:33:02 -0400

    Changed code to reflect my dropbox link
    https://www.dropbox.com/s/045rx5s31nmvxde/export.pkl?dl=0
    https://www.dropbox.com/s/045rx5s31nmvxde/export.pkl?dl=1

--- app/server.py
+++ app/server.py
@@ -9,10 +9,11 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1' #CHANGED TO BELOW
+export_file_url = 'https://www.dropbox.com/s/045rx5s31nmvxde/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['mace', 'fury']
 path = Path(__file__).parent
 
 app = Starlette()
